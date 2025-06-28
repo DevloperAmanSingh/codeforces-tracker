dc978e296ba54d645d6d494be563d490e5cbd436
DevloperAmanSingh <amansinghneh211205@gmail.com>
2025-06-29 00:23:41 +0530

    test

--- cf-tracker-backend/src/controllers/admin.controller.ts
+++ cf-tracker-backend/src/controllers/admin.controller.ts
@@ -7,6 +7,7 @@ import {
 } from "../services/scheduler.service";
 
 
+
 export const getCronSettings = async (_: Request, res: Response) => {
   const settings = await prisma.adminSettings.findFirst();
   res.json(settings || { cronExpression: "0 2 * * *", enabled: true });
